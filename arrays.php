9c2e17e435b73cffd340cf9750fa1ff6b49d5355
vincy <vincyt123@gmail.com>
2019-06-03 12:04:58 +0530

    first assignment

---
+++ arrays.php
@@ -0,0 +1,21 @@
+<?php
+
+$user = [
+	
+	'Name' => "Arun",
+	'Gender' => "Male",
+	'Age' => 18,
+	'Qualification' => "Graduate",
+	'Email Id' => "arun@gmail.com"
+
+];
+
+
+echo "USER DETAILS<br>";
+echo "Name : ".$user['Name']."<br>";
+echo "Gender : ".$user['Gender']."<br>";
+echo "Age : ".$user['Age']."<br>";
+echo "Qualification : ".$user['Qualification']."<br>";
+echo "Email Id : ".$user['Email Id'];
+
+?>
---
+++ for.php
@@ -0,0 +1,8 @@
+<?php
+
+$subject = ['Maths','English','Science','Social','Malayalam'];
+for ($i=0; $i < 5; $i++) { 
+	echo  $i+1 . " . {$subject[$i]} <br>";
+}
+
+?>
